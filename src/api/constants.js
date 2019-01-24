3ef9560e003acdfb1c0e10482c3ba10059b147ed
Adrien Pyke <adpyke@gmail.com>
2019-01-23 19:28:49 -0500

    constants refactor

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,46 +1,14 @@
-const tabBarIOSHeight = 54;
-const headerHeight = 64;
-const listItemHeight = 44;
-const fontSizeStandard = 14;
-const fontSizeMedium = 16;
-const fontSizeLarge = 18;
-const iconSizeLarge = 28;
-const contentBackgroundColor = '#fff';
-const primaryColor = 'rgba(247,247,247,1.0)';
-const primaryColorDark = '#F2F3F7';
-const textColor = '#000';
-const textColorSecondary = '#919191';
-const textColorActive = '#007AFF';
-const textColorDisabled = '#E5E5E5';
-
-export default {
-	tabBarIOSHeight,
-	headerHeight,
-	fontSizeStandard,
-	fontSizeMedium,
-	fontSizeLarge,
-	iconSizeLarge,
-	contentBackgroundColor,
-	primaryColor,
-	primaryColorDark,
-	textColor,
-	textColorSecondary,
-	textColorActive,
-	textColorDisabled
-};
-export {
-	tabBarIOSHeight,
-	headerHeight,
-	listItemHeight,
-	fontSizeStandard,
-	fontSizeMedium,
-	fontSizeLarge,
-	iconSizeLarge,
-	contentBackgroundColor,
-	primaryColor,
-	primaryColorDark,
-	textColor,
-	textColorSecondary,
-	textColorActive,
-	textColorDisabled
-};
+export const tabBarIOSHeight = 54;
+export const headerHeight = 64;
+export const listItemHeight = 44;
+export const fontSizeStandard = 14;
+export const fontSizeMedium = 16;
+export const fontSizeLarge = 18;
+export const iconSizeLarge = 28;
+export const contentBackgroundColor = '#fff';
+export const primaryColor = 'rgba(247,247,247,1.0)';
+export const primaryColorDark = '#F2F3F7';
+export const textColor = '#000';
+export const textColorSecondary = '#919191';
+export const textColorActive = '#007AFF';
+export const textColorDisabled = '#E5E5E5';
