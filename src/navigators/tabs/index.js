a74384da687d03e070d2d44de3f7dad0377aa631
Adrien Pyke <adpyke@gmail.com>
2019-02-01 15:13:34 -0500

    update icons

--- src/navigators/tabs/index.js
+++ src/navigators/tabs/index.js
@@ -1,8 +1,8 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types, react/display-name */
 import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { iconSizeTabs } from '../../api/constants';
 import Beasts from './beasts';
 import Homebrew from './homebrew';
 import Settings from './settings';
@@ -11,19 +11,19 @@ export default createBottomTabNavigator({
 	Beasts: {
 		screen: Beasts,
 		navigationOptions: {
-			tabBarIcon: ({ tintColor }) => <Icon name='ios-star' size={17} color={tintColor} />
+			tabBarIcon: ({ tintColor }) => <Icon name='ios-paw' size={iconSizeTabs} color={tintColor} />
 		}
 	},
 	Homebrew: {
 		screen: Homebrew,
 		navigationOptions: {
-			tabBarIcon: ({ tintColor }) => <Icon name='ios-star' size={17} color={tintColor} />
+			tabBarIcon: ({ tintColor }) => <Icon name='ios-construct' size={iconSizeTabs} color={tintColor} />
 		}
 	},
 	Settings: {
 		screen: Settings,
 		navigationOptions: {
-			tabBarIcon: ({ tintColor }) => <Icon name='ios-star' size={17} color={tintColor} />
+			tabBarIcon: ({ tintColor }) => <Icon name='ios-settings' size={iconSizeTabs} color={tintColor} />
 		}
 	}
 }, {
