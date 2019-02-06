1255a7bdb8e8c8703a8e58a2520edf4cb81b01a4
Adrien Pyke <adpyke@gmail.com>
2019-02-05 22:25:17 -0500

    use react-native-elements button for toggle buttons

--- src/components/shared/toggle-icon-button.js
+++ src/components/shared/toggle-icon-button.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { textColorSecondary, textColor, iconSizeLarge } from '../../api/constants';
 
 export default function ToggleIconButton(props) {
-	const button = (
-		<Icon
-			name={props.icon}
-			size={props.size || iconSizeLarge}
-			color={
-				props.active
-					? (props.activeColor || textColor)
-					: (props.inactiveColor || textColorSecondary)}
+	return (
+		<Button
+			type='clear'
+			icon={
+				<Icon
+					name={props.icon}
+					size={props.size || iconSizeLarge}
+					color={
+						props.active
+							? (props.activeColor || textColor)
+							: (props.inactiveColor || textColorSecondary)}
+				/>
+			}
+			onPress={() => props.onToggle && props.onToggle(!props.active)}
 		/>
-	);
-	const handlePress = () => props.onToggle && props.onToggle(!props.active);
-	return props.noFeedback ? (
-		<TouchableWithoutFeedback onPress={handlePress}>
-			{button}
-		</TouchableWithoutFeedback>
-	) : (
-		<TouchableOpacity onPress={handlePress}>
-			{button}
-		</TouchableOpacity>
 	);
 }
 ToggleIconButton.propTypes = {
@@ -32,6 +28,5 @@ ToggleIconButton.propTypes = {
 	icon: PropTypes.string,
 	size: PropTypes.number,
 	activeColor: PropTypes.string,
-	inactiveColor: PropTypes.string,
-	noFeedback: PropTypes.bool
+	inactiveColor: PropTypes.string
 };
