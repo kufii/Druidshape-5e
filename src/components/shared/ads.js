3868bc2cce5afe6a34e1d97c940288990851e5f5
Adrien Pyke <adpyke@gmail.com>
2019-02-12 20:53:27 -0500

    still doesn't work idk

--- src/components/shared/ads.js
+++ src/components/shared/ads.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { AdMobBanner } from 'expo';
 
-const APP_ID = 'ca-app-pub-3142513244439092~7655052957';
 const AD_UNIT_ID = 'ca-app-pub-3142513244439092/3434227600';
 
 export default class BannerAd extends React.Component {
 	render() {
 		return (
-			<View stye={styles.container}>
+			<View style={styles.container}>
 				<AdMobBanner
 					style={styles.bottomBanner}
 					bannerSize='fullBanner'
 					adUnitId={AD_UNIT_ID}
-					testDeviceID={AdMobBanner.simulatorId}
-					didFailToReceiveAdWithError={this.bannerError}
+					testDeviceID='EMULATOR'
+					didFailToReceiveAdWithError={e => console.log(e)}
 				/>
 			</View>
 		);
@@ -25,8 +24,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#333',
-		alignItems: 'center',
-		marginTop: 30
+		alignItems: 'center'
 	},
 	bottomBanner: {
 		position: 'absolute',
