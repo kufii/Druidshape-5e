a5c734fa838c834fbe674321fa66d8d05b64acf1
Adrien Pyke <adpyke@gmail.com>
2019-02-18 13:08:45 -0500

    fix search when all is selected

--- src/api/beasts.js
+++ src/api/beasts.js
@@ -3,20 +3,20 @@ export const getModifier = num => Math.floor((num - 10) / 2);
 export const crToNum = cr => cr.toString().includes('/') ? 1 / parseInt(cr.split('/')[1]) : parseInt(cr);
 
 export const filterBeasts = (beasts, level=0, circleOfTheMoon=false, search) => {
-	if (level <= 0) return beasts.slice();
-	if (level === 1) return [];
+	let filtered = beasts.slice();
+	if (level >= 2) {
+		const maxCr = circleOfTheMoon
+			? (level < 6 ? 1 : Math.floor(level / 3))
+			: (level < 4
+				? (1 / 4)
+				: (level < 8 ? (1 / 2) : 1));
+		const canSwim = level >= 4;
+		const canFly = level >= 8;
 
-	const maxCr = circleOfTheMoon
-		? (level < 6 ? 1 : Math.floor(level / 3))
-		: (level < 4
-			? (1 / 4)
-			: (level < 8 ? (1 / 2) : 1));
-	const canSwim = level >= 4;
-	const canFly = level >= 8;
-
-	let filtered = beasts.slice().filter(({ cr }) => crToNum(cr) <= maxCr);
-	if (!canSwim) filtered = filtered.filter(b => !b.swim);
-	if (!canFly) filtered = filtered.filter(b => !b.fly);
+		filtered = filtered.filter(({ cr }) => crToNum(cr) <= maxCr);
+		if (!canSwim) filtered = filtered.filter(b => !b.swim);
+		if (!canFly) filtered = filtered.filter(b => !b.fly);
+	}
 	if (search) filtered = filtered.filter(b => b.name.toLowerCase().includes(search.toLowerCase()));
 
 	return filtered;
