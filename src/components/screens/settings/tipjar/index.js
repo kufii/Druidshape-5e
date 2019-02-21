4709578b879d621d5d74f05895885f6c1c32d81d
Adrien Pyke <adpyke@gmail.com>
2019-02-21 11:34:23 -0500

    tip jar styling

--- src/components/screens/settings/tipjar/index.js
+++ src/components/screens/settings/tipjar/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import Toast from 'react-native-root-toast';
 import * as RNIap from 'react-native-iap';
 import { ListItem, Divider } from 'react-native-elements';
+import { fontSizeMedium } from '../../../../api/constants';
 import listStyles from '../../../../styles/list';
 
 export default class SettingsScreen extends React.Component {
@@ -24,7 +25,17 @@ export default class SettingsScreen extends React.Component {
 		return StyleSheet.create({
 			container: {
 				flex: 1,
-				backgroundColor: theme.contentBackgroundColorDark
+				backgroundColor: theme.contentBackgroundColor
+			},
+			disclaimer: {
+				color: theme.textColor,
+				fontSize: fontSizeMedium,
+				padding: 10,
+				flexWrap: 'wrap'
+			},
+			badge: {
+				backgroundColor: theme.formButtonColor,
+				borderColor: 'transparent'
 			}
 		});
 	}
@@ -37,19 +48,24 @@ export default class SettingsScreen extends React.Component {
 
 		return (
 			<View style={styles.container}>
+				<Text style={styles.disclaimer}>
+					{"If you've been enjoying Druidshape, and would like to show your support, please consider leaving a tip. This is obviously not mandatory and just the fact that you're using my app is extremely appreciated. Thanks! :)"}
+				</Text>
 				<FlatList
 					data={state.iaps.filter(p => p.productId.match(/^com\.adpyke\.druidshape\.tip\./))}
-					renderItem={({ productId, title, localizedPrice }) => (
+					renderItem={({ item: { productId, title, localizedPrice } }) => (
 						<ListItem
 							title={title}
-							badge={{ value: localizedPrice }}
+							badge={{ value: localizedPrice, badgeStyle: styles.badge }}
 							titleStyle={listTheme.itemText}
 							containerStyle={listTheme.item}
 							onPress={() => RNIap.buyProduct(productId).then(() => Toast.show('Thank for you supporting Druidshape!'))}
 						/>
 					)}
 					keyExtractor={item => item.productId}
+					ListHeaderComponent={() => <Divider style={listTheme.divider} />}
 					ItemSeparatorComponent={() => <Divider style={listTheme.divider} />}
+					ListFooterComponent={() => <Divider style={listTheme.divider} />}
 				/>
 			</View>
 		);
