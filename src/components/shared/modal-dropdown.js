351fa4d578f5d3b1904ecd04482c262d5b8d59f0
Adrien Pyke <adpyke@gmail.com>
2019-04-11 10:54:57 -0400

    use hooks for ModalDropdown

--- src/components/shared/modal-dropdown.js
+++ src/components/shared/modal-dropdown.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ViewPropTypes, Platform, StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import Modal from 'react-native-modal';
@@ -9,82 +9,70 @@ import listStyles from '../../styles/list';
 
 import { fontSizeMedium, fontSizeLarge } from '../../api/constants';
 
-export default class ModalDropdown extends React.Component {
-	state = { visible: false };
+export default function ModalDropdown({ actions, items, selected, onSelect, style }) {
+	const [isVisible, setVisible] = useState(false);
 
-	static propTypes = {
-		actions: PropTypes.object.isRequired,
-		items: PropTypes.arrayOf(PropTypes.shape({
-			key: PropTypes.string.isRequired,
-			text: PropTypes.string.isRequired
-		})).isRequired,
-		selected: PropTypes.string,
-		onSelect: PropTypes.func,
-		style: ViewPropTypes.style
-	};
+	const getOptionText = key => items.find(i => i.key === key).text;
 
-	get styles() {
-		const theme = this.props.actions.getCurrentTheme();
-		return StyleSheet.create({
-			container: {
-				flex: 1,
-				backgroundColor: theme.contentBackgroundColor
-			},
-			dropdown: {
-				flex: 1,
-				flexDirection: 'row',
-				alignItems: 'center'
-			},
-			text: {
-				fontWeight: 'bold',
-				marginRight: 4,
-				fontSize: Platform.OS === 'ios' ? fontSizeMedium : fontSizeLarge,
-				color: theme.headerTextColor
-			}
-		});
-	}
+	const theme = actions.getCurrentTheme();
 
-	closeModal() {
-		this.setState({ visible: false });
-	}
+	const styles = StyleSheet.create({
+		container: {
+			flex: 1,
+			backgroundColor: theme.contentBackgroundColor
+		},
+		dropdown: {
+			flex: 1,
+			flexDirection: 'row',
+			alignItems: 'center'
+		},
+		text: {
+			fontWeight: 'bold',
+			marginRight: 4,
+			fontSize: Platform.OS === 'ios' ? fontSizeMedium : fontSizeLarge,
+			color: theme.headerTextColor
+		}
+	});
+	const listTheme = listStyles(theme);
 
-	getOptionText(key) {
-		return this.props.items.find(i => i.key === key).text;
-	}
-
-	render() {
-		const theme = this.props.actions.getCurrentTheme();
-		const styles = this.styles;
-		const listTheme = listStyles(theme);
-		return (
-			<>
-				<TouchableOpacity onPress={() => this.setState({ visible: true })}>
-					<View style={[styles.dropdown, this.props.style]}>
-						<Text style={styles.text}>{this.getOptionText(this.props.selected)}</Text>
-						<Icon name='ios-arrow-down' size={fontSizeMedium} color={theme.headerTextColor} />
-					</View>
-				</TouchableOpacity>
-				<Modal isVisible={this.state.visible} onBackdropPress={() => this.closeModal()} onBackButtonPress={() => this.closeModal()}>
-					<View style={styles.container}>
-						<FlatList
-							data={this.props.items}
-							renderItem={({ item }) => (
-								<TouchableOpacity
-									onPress={() => {
-										this.props.onSelect && this.props.onSelect(item.key);
-										this.closeModal();
-									}}
-								>
-									<View style={listTheme.itemCompact}>
-										<Text style={listTheme.itemText}>{item.text}</Text>
-									</View>
-								</TouchableOpacity>
-							)}
-							ItemSeparatorComponent={() => <Divider style={listTheme.divider} />}
-						/>
-					</View>
-				</Modal>
-			</>
-		);
-	}
+	return (
+		<>
+			<TouchableOpacity onPress={() => setVisible(true)}>
+				<View style={[styles.dropdown, style]}>
+					<Text style={styles.text}>{getOptionText(selected)}</Text>
+					<Icon name='ios-arrow-down' size={fontSizeMedium} color={theme.headerTextColor} />
+				</View>
+			</TouchableOpacity>
+			<Modal isVisible={isVisible} onBackdropPress={() => setVisible(false)} onBackButtonPress={() => setVisible(false)}>
+				<View style={styles.container}>
+					<FlatList
+						data={items}
+						renderItem={({ item }) => (
+							<TouchableOpacity
+								onPress={() => {
+									onSelect && onSelect(item.key);
+									setVisible(false);
+								}}
+							>
+								<View style={listTheme.itemCompact}>
+									<Text style={listTheme.itemText}>{item.text}</Text>
+								</View>
+							</TouchableOpacity>
+						)}
+						ItemSeparatorComponent={() => <Divider style={listTheme.divider} />}
+					/>
+				</View>
+			</Modal>
+		</>
+	);
 }
+ModalDropdown.propTypes = {
+	actions: PropTypes.object.isRequired,
+	items: PropTypes.arrayOf(PropTypes.shape({
+		key: PropTypes.string.isRequired,
+		text: PropTypes.string.isRequired
+	})).isRequired,
+	selected: PropTypes.string,
+	onSelect: PropTypes.func,
+	style: ViewPropTypes.style
+};
