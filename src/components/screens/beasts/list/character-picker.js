37ab78e37afbdb7ad9d238fa258f4048b1db321e
Adrien Pyke <adpyke@gmail.com>
2019-04-11 00:00:19 -0400

    add new character button ui

--- src/components/screens/beasts/list/character-picker.js
+++ src/components/screens/beasts/list/character-picker.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
-import { ListItem, Divider } from 'react-native-elements';
+import { ListItem, Divider, Button } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import Modal from 'react-native-modal';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -21,6 +21,12 @@ export default function CharacterPicker(props) {
 		modal: {
 			margin: 0,
 			justifyContent: 'flex-end'
+		},
+		header: {
+			flexDirection: 'row',
+			justifyContent: 'flex-end',
+			backgroundColor: theme.contentBackgroundColor,
+			paddingRight: 10
 		}
 	});
 	const listTheme = listStyles(theme);
@@ -33,6 +39,12 @@ export default function CharacterPicker(props) {
 			style={styles.modal}
 		>
 			<View style={styles.container}>
+				<View style={styles.header}>
+					<Button
+						type='clear'
+						icon={<Icon size={iconSizeLarge} color={theme.formButtonColor} name={icon('add')} />}
+					/>
+				</View>
 				<FlatList
 					data={state.characters}
 					renderItem={({ item }) => (
@@ -52,6 +64,7 @@ export default function CharacterPicker(props) {
 					)}
 					keyExtractor={c => c.key.toString()}
 					ItemSeparatorComponent={() => <Divider style={listTheme.divider} />}
+					ListHeaderComponent={() => <Divider style={listTheme.divider} />}
 				/>
 			</View>
 		</Modal>
