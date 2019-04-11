a69cb6a7d7d34e560f25ea99959d4b3cd34d2fdf
Adrien Pyke <adpyke@gmail.com>
2019-04-11 10:43:45 -0400

    update react-native to 0.59

--- android/app/src/main/java/com/adpyke/druidshape/MainApplication.java
+++ android/app/src/main/java/com/adpyke/druidshape/MainApplication.java
@@ -3,6 +3,7 @@ package com.adpyke.druidshape;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.dooboolab.RNIap.RNIapPackage;
 import com.rnfs.RNFSPackage;
 import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new RNIapPackage(),
             new RNFSPackage(),
             new ReactNativeDocumentPicker(),
