a94ca06b0e92cbe40ed63e756911bb7c1cb37fd3
Adrien Pyke <adpyke@gmail.com>
2019-04-12 15:55:37 -0400

    use LayoutAnimation.create

--- src/api/util.js
+++ src/api/util.js
@@ -68,13 +68,7 @@ export const fabOnScroll = (cbGetFabVisible, cbSetFabVisible, cbGetListHeight, c
 	let listViewOffset = 0;
 	return event => {
 		// Simple fade-in / fade-out animation
-		const animProp = { type: LayoutAnimation.Types.linear, property: LayoutAnimation.Properties.opacity };
-		const CustomLayoutLinear = {
-			duration: 100,
-			create: animProp,
-			update: animProp,
-			delete: animProp
-		};
+		const CustomLayoutLinear = LayoutAnimation.create(100, 'linear', 'opacity');
 		// Check if the user is scrolling up or down by confronting the new scroll position with your own one
 		const limit = cbGetListContentHeight() - cbGetListHeight();
 		const offset = event.nativeEvent.contentOffset.y;
