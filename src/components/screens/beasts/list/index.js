2764684dfcd9ed025a38d0d8b6a4f76233934de1
Adrien Pyke <adpyke@gmail.com>
2019-04-12 16:03:59 -0400

    remove fixNativeFeedbackRadius

--- src/components/screens/beasts/list/index.js
+++ src/components/screens/beasts/list/index.js
@@ -148,7 +148,6 @@ export default withCollapsible(class BeastListScreen extends React.Component {
 				/>
 				{this.state.isFabVisible && (
 					<ActionButton
-						fixNativeFeedbackRadius
 						onPress={() => this.setState({ isCharacterPickerVisible: true })}
 						degrees={0}
 						buttonColor={theme.fabColor}
