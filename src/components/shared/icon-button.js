245c15d8e9c965bacaa1dac6816f662966bdf395
Adrien Pyke <adpyke@gmail.com>
2019-05-05 11:57:10 -0400

    allow for changing styles for Icon buttons

--- src/components/shared/icon-button.js
+++ src/components/shared/icon-button.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ViewPropTypes } from 'react-native';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
 import buttonStyles from '../../styles/buttons';
 
-export default function IconButton({ icon, size, color, ...props }) {
+export default function IconButton({ icon, size, color, buttonStyle, containerStyle, ...props }) {
 	return (
 		<Button
-			buttonStyle={buttonStyles.icon.buttonStyle}
-			containerStyle={buttonStyles.icon.containerStyle}
+			buttonStyle={[buttonStyles.icon.buttonStyle, buttonStyle]}
+			containerStyle={[buttonStyles.icon.containerStyle, containerStyle]}
 			type='clear'
 			icon={
 				<Icon
@@ -24,5 +25,7 @@ export default function IconButton({ icon, size, color, ...props }) {
 IconButton.propTypes = {
 	icon: PropTypes.string,
 	size: PropTypes.number,
-	color: PropTypes.string
+	color: PropTypes.string,
+	buttonStyle: ViewPropTypes.style,
+	containerStyle: ViewPropTypes.style
 };
