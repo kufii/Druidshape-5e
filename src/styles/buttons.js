498b386ce24bb51b8df5dcebf60e86bc93391ac8
Adrien Pyke <adpyke@gmail.com>
2019-09-11 00:07:34 -0400

    foramatting

--- src/styles/buttons.js
+++ src/styles/buttons.js
@@ -6,19 +6,19 @@ export const rebuild = () =>
 		icon: {
 			buttonStyle: r`ar 1`,
 			containerStyle: r`
-			br 999999
-			jc center
-			ai center
-			o hidden
-		`
+				br 999999
+				jc center
+				ai center
+				o hidden
+			`
 		},
 		bottom: {
 			container: r`
-			fd row
-			jc space-between
-			btc $formButtonColor
-			btw hw
-		`,
+				fd row
+				jc space-between
+				btc $formButtonColor
+				btw hw
+			`,
 			button: r`w 50%; br 0`,
 			cancelButton: r`bc $formButtonColorSecondary`,
 			cancelButtonTitle: r`c $formButtonColor`,
