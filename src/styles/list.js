f0bc13e5c4c5490680ba554f2b9f7395b0b9b4e8
Adrien Pyke <adpyke@gmail.com>
2019-09-11 00:08:10 -0400

    foramtting

--- src/styles/list.js
+++ src/styles/list.js
@@ -4,17 +4,17 @@ let listStyles;
 export const rebuild = () =>
 	(listStyles = {
 		sectionHeader: r`
-		p 2 10
-		fs $fontSizeStandard
-		fw bold
-		bc $contentBackgroundColorDark
-		c $textColorSecondary
-	`,
+			p 2 10
+			fs $fontSizeStandard
+			fw bold
+			bc $contentBackgroundColorDark
+			c $textColorSecondary
+		`,
 		item: r`
-		pt 0; pb 0
-		h $listItemHeight
-		bc $contentBackgroundColor
-	`,
+			pt 0; pb 0
+			h $listItemHeight
+			bc $contentBackgroundColor
+		`,
 		itemText: r`fs $fontSizeMedium; c $textColor`,
 		divider: r`bc $dividerColor`
 	});
